bda23906e647d04d4374da61ba478578c0576124
morihirok <omnimani3@gmail.com>
2021-01-26 09:43:45 +0900

    構成にMongoDBを追加

---
+++ webapp/mongodb/db/init.sh
@@ -0,0 +1,2 @@
+mongoimport --username=isucon --password=isucon --authenticationDatabase=admin --db=isuumo --collection=estate --type=csv --file=/csvfiles/1_DummyEstateData.csv --headerline
+mongoimport --username=isucon --password=isucon --authenticationDatabase=admin --db=isuumo --collection=chair --type=csv --file=/csvfiles/2_DummyChairData.csv --headerline
